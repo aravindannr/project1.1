691a150f1b392842f60c1ae68e97a16e603a743d
harikrishnan <kphari891@gmail.com>
2022-11-22 16:17:45 +0530

    Initial commit

---
+++ bin/project1.dart
@@ -0,0 +1,30 @@
+void main(){
+   add();
+   sub();
+   mul();
+   div();
+}
+void add(){
+  int a=2,
+       b=3,
+  sum=a+b;
+  print('sum=$sum');
+}
+void sub(){
+  int a=2,
+      b=3,
+  sub=a-b;
+  print('sub=$sub');
+}
+void mul(){
+  int a=2,
+      b=3,
+  mul=a*b;
+  print('mul=$mul');
+}
+void div(){
+  int a=2,
+      b=3,
+  div=a~/b;
+  print('div=$div');
+}
